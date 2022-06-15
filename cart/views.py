781eafd1f32b6add3ac9bfc8c8bfab0ba44efbf9
root <root@Testpress.localdomain>
2022-06-15 18:52:11 +0530

    Fix cart add bug

--- cart/views.py
+++ cart/views.py
@@ -8,6 +8,7 @@ from .forms import CartAddProductForm
 
 @require_POST
 def cart_add(request, product_id):
+    print()
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     form = CartAddProductForm(request.POST)
@@ -25,4 +26,4 @@ def cart_remove(request,product_id):
 
 def cart_detail(request):
     cart = Cart(request)
-    return render(request, 'cart/detail.html', {'cart': cart})
+    return render(request, 'cart/details.html', {'cart': cart})
