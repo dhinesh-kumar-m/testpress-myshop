a3b2d7d2218cd49bfdb85d92379daf4c171064a8
root <root@Testpress.localdomain>
2022-06-16 19:34:21 +0530

    Add payment complete task

--- orders/tasks.py
+++ orders/tasks.py
@@ -1,6 +1,12 @@
+from io import BytesIO
 from celery import shared_task
 from django.core.mail import send_mail
 from .models import Order
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+from orders.models import Order
+import weasyprint
 
 @shared_task
 def order_created(order_id):
@@ -15,3 +21,26 @@ def order_created(order_id):
             f'Your order ID is {order.id}'
     mail_send = send_mail(subject,message,'admin@myshop.com',[order.email])
     return mail_send
+
+@shared_task
+def payment_completed(order_id):
+    """
+    Task to send an e-mail notification when an order is 
+    successfully created
+    """
+    order = Order.objects.get(id=order_id)
+
+    # create invoice e-mail 
+    subject = f'My Shop - EE Invoice no. {order.id}'
+    message = 'Please, find attach the invoice for your recent purchase.'
+    email = EmailMessage(subject,message,'admin@myshop.com',[order.email])
+    # generate PDF
+    html = render_to_string('orders/order/pdf.html',{'order':order})
+    out = BytesIO()
+    stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+    weasyprint.HTML(string=html).write_pdf(out,stylesheets=stylesheets)
+
+    #attach PDF file
+    email.attach(f'order_{order.id}.pdf',out.getvalue(),'application/pdf')
+    email.send()
+    
