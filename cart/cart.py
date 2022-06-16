60f9f427320eced103413dc4c81badbf73cc7f42
root <root@Testpress.localdomain>
2022-06-16 12:13:02 +0530

    Fix cart module bug

--- cart/cart.py
+++ cart/cart.py
@@ -8,7 +8,11 @@ class Cart(object):
         Initialize the cart.
         """
         self.session = request.session
-        self.cart = self.session.get(settings.CART_SESSION_ID,{})
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            # save an empty cart in the session
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
 
     def add(self, product, quantity=1, override_quantity=False):
         """
