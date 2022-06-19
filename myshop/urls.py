ebaee1f6c3e00a9d792e8ba8d2363ce621c61c1e
Dhinesh <dinesh.k131997@gmail.com>
2022-06-19 21:12:34 +0530

    Fix translation error

--- myshop/urls.py
+++ myshop/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('payment/', include('payment.urls', namespace='payment')),
@@ -26,6 +27,6 @@ urlpatterns = [
     path('rosetta/', include('rosetta.urls')),
     path('', include('shop.urls', namespace='shop')),
     path('orders/', include('orders.urls', namespace='orders')),
-]
+)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
