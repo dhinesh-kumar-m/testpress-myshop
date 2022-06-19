a43875c570d92ff7dc7151c09ea4ab972ba191f8
Dhinesh <dinesh.k131997@gmail.com>
2022-06-19 21:21:42 +0530

    Translate urls

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from django.utils.translation import gettext_lazy as _
 
 app_name = "cart"
 urlpatterns = [
-    path("", views.cart_detail, name="cart_detail"),
-    path("add/<int:product_id>/", views.cart_add, name="cart_add"),
-    path("remove/<int:product_id>/", views.cart_remove, name="cart_remove"),
+    path(_(""), views.cart_detail, name="cart_detail"),
+    path(_("add/<int:product_id>/"), views.cart_add, name="cart_add"),
+    path(_("remove/<int:product_id>/"), views.cart_remove, name="cart_remove"),
 ]
